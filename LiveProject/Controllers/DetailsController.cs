8251bf09122dd89e929766b8053cc01effb44896
rsatabdirouth <satabdi.emedia@outlook.com>
2017-05-22 19:26:00 +0530

    initial

---
+++ LiveProject/Controllers/DetailsController.cs
@@ -0,0 +1,32 @@
+﻿using LiveProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LiveProject.Controllers
+{
+    public class DetailsController : Controller
+    {
+        private TopMortgageEntities _db = new TopMortgageEntities();
+        // GET: Details
+        public ActionResult Index()
+        {
+            CustDetail model = new CustDetail();
+            return View(model);
+        }
+        public ActionResult Cust(CustDetail model)
+        {
+            _db.CustDetails.Add(model);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Requestacallback(RequestaCallBack model)
+        {
+            _db.RequestaCallBacks.Add(model);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ LiveProject/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using LiveProject.Models;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+
+
+namespace LiveProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private TopMortgageEntities _db = new TopMortgageEntities();
+        // GET: Home
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            CustDetail model = new CustDetail();
+            return View(model);
+        }
+
+
+        [HttpPost]
+        public ActionResult Cust(CustDetail model)
+        {
+            _db.CustDetails.Add(model);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult RequestCallBack(RequestaCallBack model)
+        {
+            _db.RequestaCallBacks.Add(model);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
+
+    
